c2b951070c1349c91e06a817234416c311171909
w. chorolque <wchorolque@gmail.com>
2013-04-20 12:30:59 -0400

    Creado Librerias de Proyectos

--- SimpleAdminPais/Program.cs
+++ SimpleAdminPais/Program.cs
@@ -15,7 +15,7 @@ namespace SimpleAdminPais
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run();
         }
     }
 }
